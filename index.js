e3f620391a66f16a186a99cd9985023a180710ff
tamminen.mikkoj@gmail.com <tamminen.mikkoj@gmail.com>
2018-09-05 16:28:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
 function happyHolidaysTo(holiday, name) {
-  return `Happy holidays, ${name}!`
+  return `Happy ${holiday}, ${name}!`
 }
